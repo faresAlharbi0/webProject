b2c9eccdcf5da68401934b1b6174a845196d7ffd
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-01 19:33:27 +0300

    fixes (fares)

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,6 +1,7 @@
 const dropicon = document.getElementById("dropicon")
 dropicon.addEventListener('click',dropDownMenu);
 const mcontainer = document.getElementById("mcontainer");
+mcontainer.classList.toggle("is-active")
 function dropDownMenu(){
     mcontainer.classList.toggle("is-active")
 }
