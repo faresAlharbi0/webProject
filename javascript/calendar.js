1e58f68fec76985947f9faddc418608fb33fcbbd
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-02 13:32:24 +0300

    .

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,7 +1,20 @@
+open = true;
+const tl = gsap.timeline({duration: 1});
 const dropicon = document.getElementById("dropicon")
 dropicon.addEventListener('click',dropDownMenu);
 const mcontainer = document.getElementById("mcontainer");
-mcontainer.classList.toggle("is-active")
+//works here//
+tl.to(mcontainer,{height:"100%"}, 0);
 function dropDownMenu(){
-    mcontainer.classList.toggle("is-active")
+    if(open === true){
+        mcontainer.classList.toggle("is-active");
+        open = false;
+        //doesn't work here//
+        tl.to(mcontainer,{height:"100%"}, 0);
+    }
+    if(open === false){
+        tl.to(mcontainer,{height:"0"}, "<");
+        open = true;
+    }
+
 }
