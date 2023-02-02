7abd2f52ef63521b76752aef0b94be8c7ed15c11
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-02 17:17:54 +0300

    fixes fares

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,22 +1,27 @@
 open = true;
+animating = false;
 const tl = gsap.timeline().from(".is-active",{yPercent:-50,opacity:0,});
 const tl2 = gsap.timeline().fromTo(".is-active",{opacity:1},{opacity:0,duration: 0.3});
 const dropicon = document.getElementById("dropicon")
-dropicon.addEventListener('click',dropDownMenu);
 const mcontainer = document.getElementById("mcontainer");
-mcontainer.classList.toggle("is-active")
+mcontainer.classList.toggle("is-active");
+dropicon.addEventListener('click',dropDownMenu);
 //works here//
 function dropDownMenu(){
     if(open === true){
         mcontainer.classList.toggle("is-active")
         open = false;
         //doesn't work here//
-        tl.play(0);
+        if(!tl.isActive() && !tl2.isActive()){
+            tl.play(0);
+        }
         console.log(1);
         return;
     } 
     if(open === false){
-        tl2.play(0);
+        if((!tl.isActive()) && (!tl2.isActive())){
+            tl2.play(0);
+        }
         open = true;
         console.log(2);
         setTimeout(()=>mcontainer.classList.toggle("is-active"),1000);
