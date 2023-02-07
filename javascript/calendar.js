6340dc5ea55f393f947b049d5dca87cac530765d
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-07 20:05:52 +0300

    added next days and current day (fares)

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -15,7 +15,9 @@ const month = [
   "December",
 ];
 const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+const lastDayIndex = new Date(date.getFullYear(),date.getMonth() + 1,0).getDay();
 const firstDayIndex = date.getDay();
+const nextDays = 7 - lastDayIndex -1;
 document.querySelector(".date h1").innerHTML = month[date.getMonth()];
 document.querySelector(".date p").innerHTML = date.toDateString();
 let days = "";
@@ -23,7 +25,15 @@ for(let x = firstDayIndex +1; x>0; x--){
   days += "<div class='prev-date'>"+(prevLastDay-x+1) +"</div>";
 }
 for(let i =1; i<=lastDate; i++){
-  days += "<div>"+i+"</div>";
+  if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+    days += "<div class='today'>"+i+"</div>";
+    console.log("was here");
+  }else{
+    days += "<div>"+i+"</div>";
+  }
+}
+for(let j = 1; j <= nextDays; j++){
+  days += "<div class='next-date'>"+j+"</div>";
 }
 monthDays.innerHTML = days;
 //drop menue animation
