b1c47fab64c78fc9360305a262f237a28060b3c3
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-08 16:53:49 +0300

    adedd names

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 console.log("bbbb");
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({extended:false}));
 app.use("/",express.static("./website"));
+app.post("/login",(request,response)=>{
+    const email = request.body.email;
+    const passward = request.body.password;
+    let msg = "<h1>"+email+"</h1>" + "<h1>"+passward+"</h1>";
+    response.send(msg);
+})
 app.listen(4000, ()=>{
     console.log("running");
 
