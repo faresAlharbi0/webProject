60600100f402f01e0a3ea1bd3e8aa7900892841a
AbdulazizAlrizqi <d-07123@hotmail.com>
2023-02-09 21:10:04 +0300

    make changes on about us page and contact us page and add js file abdulaziz alrizqi 2042545

---
+++ javascript/contactUs.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#formid");
+const msg = document.querySelector("#msg");
+
+form.addEventListener('submit', e=>{
+    let wrongMessages = [];
+
+    wrongMessages = isFilled("#fname", wrongMessages,"First name is missing");
+    wrongMessages = isFilled("#lname", wrongMessages,"Last name is missing");
+    wrongMessages = isFilled("#email", wrongMessages, " email is missing");
+    wrongMessages = isEmail("#email", wrongMessages,"email is in wrong format");
+        
+
+    if(wrongMessages.length>0){
+        msg.innerHTML = wrongMessages.join(", ");
+        e.preventDefault();
+    }
+    
+   
+})
+
+function isFilled(selector, wrongMessages,msg){
+    const element = document.querySelector(selector).value.trim();
+    if (element.length<1){
+        wrongMessages.push(msg);
+    }
+    return wrongMessages;
+}
+
+function isEmail(selector, wrongMessages,msg){
+    const element = document.querySelector(selector).value.trim();
+    if (!element.match("a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}\.*[a-zA-Z]*")){
+        wrongMessages.push(msg);
+    }
+    return wrongMessages;
+}
