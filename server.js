fba02c83e440156411a16928082daf09be2cb6a9
faresAlharbi0 <2040846@uj.edu.sa>
2023-02-10 17:56:15 +0300

    fixes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use("/",express.static("./website"));
-app.post("/login",(request,response)=>{
+app.post("/submitForm",(request,response)=>{
+    const fname = request.body.fname;
+    const lname = request.body.lname;
     const email = request.body.email;
-    const passward = request.body.password;
-    let msg = "<h1>"+email+"</h1>" + "<h1>"+passward+"</h1>";
+    const message = request.body.message;
+    let msg = "<h1>"+fname+"</h1>" + "<h1>"+lname+"</h1>"+"<h1>"+email+"</h1>" + "<h1>"+message+"</h1>";
     response.send(msg);
 })
 app.listen(4000, ()=>{
